e90883b4ff0d681e93ddd5fba5ef33a2277ff17d
Mahtab Ahmad <105929699+Mahtab-281@users.noreply.github.com>
2024-09-06 12:52:32 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,27 +1,23 @@
 #include <gtest/gtest.h>
-#include "Soundex.h"  
-
+#include "Soundex.h"
+ 
 TEST(SoundexTest, HandlesEmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
-
+ 
 TEST(SoundexTest, HandlesSingleCharacter) {
     for (char c = 'A'; c <= 'Z'; ++c) {
         std::string expected = std::string(1, c) + "000";
         EXPECT_EQ(generateSoundex(std::string(1, c)), expected);
     }
 }
-
+ 
 TEST(SoundexTest, HandlesIgnoredCharacters) {
     std::vector<std::pair<std::string, std::string>> testCases = {
-        {"AEIOU", "A000"},  
-        {"HW", "H000"},     
-        {"HWY", "H000"},    
-        {"BHWX", "B200"},    
-        {"SAHWEX", "S200"},  
-        {"CEGIKMOQSU", "C200"} 
+        {"AEIOU", "A000"}, {"EAIOU", "E000"}, {"IAEOU", "I000"}, 
+        {"OAEIU", "O000"}, {"UAEIO", "U000"}, {"HW", "H000"}, 
+        {"WH", "W000"}
     };
-
     for (const auto& testCase : testCases) {
         EXPECT_EQ(generateSoundex(testCase.first), testCase.second);
     }
@@ -29,10 +25,13 @@ TEST(SoundexTest, HandlesIgnoredCharacters) {
 
 TEST(SoundexTest, HandlesAdjacentConsonants) {
     std::map<char, std::string> soundexMapping = {
-        {'B', "B100"}, {'C', "C200"}, {'D', "D300"}, {'L', "L400"}, 
-        {'M', "M500"}, {'R', "R600"}
+        {'B', "B100"}, {'C', "C200"}, {'D', "D300"}, {'F', "F100"}, 
+        {'G', "G200"}, {'J', "J200"}, {'K', "K200"}, {'L', "L400"}, 
+        {'M', "M500"}, {'N', "N500"}, {'P', "P100"}, {'Q', "Q200"}, 
+        {'R', "R600"}, {'S', "S200"}, {'T', "T300"}, {'V', "V100"}, 
+        {'X', "X200"}, {'Y', "Y000"}, {'Z', "Z200"}
     };
-
+ 
     for (const auto& entry : soundexMapping) {
         char consonant = entry.first;
         std::string expected = entry.second;
